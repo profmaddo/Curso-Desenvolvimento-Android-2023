c9d387750cd70c8893329d63bfe50b2dfe2cfd25
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-23 16:01:52 -0400

    App Lista de Interessados em Curso Vip Android, Primeiro Commit.

--- AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
