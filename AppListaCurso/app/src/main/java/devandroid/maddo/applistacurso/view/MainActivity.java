0f1a96a9f3d278cb3b0f6645c2512935d51407b7
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-24 11:37:01 -0400

    Criação Classe Pessoa camada MODEL

--- AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/view/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import devandroid.maddo.applistacurso.R;
+import devandroid.maddo.applistacurso.model.Pessoa;
 
 public class MainActivity extends AppCompatActivity {
+
+    Pessoa pessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pessoa = new Pessoa();
+
+
     }
 }
