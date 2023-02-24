459d762c2325212bb3a90d452797eb291c92253e
MarcoMaddo-M1 <profmaddo@gmail.com>
2023-02-24 18:57:27 -0400

    Uso dos métodos Get e Set.

--- AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/model/Pessoa.java
+++ AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/model/Pessoa.java
@@ -5,11 +5,46 @@ package devandroid.maddo.applistacurso.model;
 public class Pessoa {
 
     // Construtor
+    // Atributos - Objetos - Molde - Modelo - Template
+    // Métodos de Acesso - Getts and Setts
 
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
 
-    public Pessoa(){
+    public Pessoa(){}
 
-
+    public String getPrimeiroNome() {
+        return primeiroNome;
     }
 
+    public void setPrimeiroNome(String primeiroNome) {
+
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
 }
--- AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/maddo/applistacurso/view/MainActivity.java
@@ -10,6 +10,10 @@ import devandroid.maddo.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa; // pascalCase - camelCase
+    String dadosOutraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         pessoa = new Pessoa();
+        // Atribuir conteúdo, dados, valores para o Objeto
+        // Conforme o seu MODELO, TEMPLATE
+        pessoa.setPrimeiroNome("Marco");
+        pessoa.setSobreNome("Maddo");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11-99229191");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobreNome("Alves");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("11-99223344");
+
+        dadosPessoa = "Primeiro nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+
+
+        int parada = 0;
+
+
 
 
     }
